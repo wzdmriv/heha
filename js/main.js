a7fb666d49c6683fb641a89d405cc16c5176d23e
Kei Kawamura <wzdmriv@g.ecc.u-tokyo.ac.jp>
2021-01-21 12:19:28 +0900

    prevent sleep

--- js/main.js
+++ js/main.js
@@ -1,18 +1,1 @@
 db.ref("/reslist").child("keylist").set("hello");
-document.addEventListener('click', function() {
-    document.getElementById('sleep-prevent-video-ios').play();
-    document.getElementById('sleep-prevent-video-android').play();
-});
-setInterval(function(){
-    document.getElementById('sleep-prevent-video-ios').play();
-    document.getElementById('sleep-prevent-video-android').play();
-    console.log("hello")
-}, 20000);
-
-iosSleepPreventInterval = setInterval(function () {
-    window.location.href = "/new/page";
-    window.setTimeout(function () {
-        window.stop()
-    }, 0);
-    console.log("hello2")
-}, 10000);
