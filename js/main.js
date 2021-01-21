0b95bffdf024a7ba5a74af59d47e0bc893722b18
Kei Kawamura <wzdmriv@g.ecc.u-tokyo.ac.jp>
2021-01-21 14:49:36 +0900

    s

--- js/main.js
+++ js/main.js
@@ -3,5 +3,6 @@ db.ref("/reslist").child("keylist").set("hello");
 function genURL(){
     var room_id = document.forms.form1.room_id.value;
     var mail_address = document.forms.form1.mail_address.value;
-    
+    var url = room_id
+    document.getElementById( "gend_url" ).value = url ;
 }
