9e2d62be2fa5115596abdbb7dfcf48a09639525c
Kei Kawamura <wzdmriv@g.ecc.u-tokyo.ac.jp>
2021-01-21 16:33:47 +0900

    s

--- js/main.js
+++ js/main.js
@@ -3,6 +3,6 @@ db.ref("/reslist").child("keylist").set("hello");
 function genURL(){
     var room_id = document.forms.form1.room_id.value;
     var mail_address = document.forms.form1.mail_address.value;
-    var url = room_id
-    document.getElementById( "gend_url" ).value = "hoge" + url ;
+    var url = "https://wzdmriv.github.io/heha/heha_button.html/?room_id=" + room_id
+    document.getElementById( "gend_url" ).value = url ;
 }
