cf0c7562bb9c5600f596c59098488b69656d0a3b
Kei Kawamura <wzdmriv@g.ecc.u-tokyo.ac.jp>
2021-01-21 12:11:44 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,3 +8,11 @@ setInterval(function(){
     document.getElementById('sleep-prevent-video-android').play();
     console.log("hello")
 }, 20000);
+
+iosSleepPreventInterval = setInterval(function () {
+    window.location.href = "/new/page";
+    window.setTimeout(function () {
+        window.stop()
+    }, 0);
+    console.log("hello2")
+}, 10000);
