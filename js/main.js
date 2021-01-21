f95c14dde909d5bf2d3438b06f1fafce36b2070d
Kei Kawamura <wzdmriv@g.ecc.u-tokyo.ac.jp>
2021-01-21 12:31:25 +0900

    s

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 db.ref("/reslist").child("keylist").set("hello");
+
+document.addEventListener('click', function() {
+    document.getElementById('sleep-prevent-video-ios').play();
+    document.getElementById('sleep-prevent-video-android').play();
+});
+
+setInterval(function(){
+    document.getElementById('sleep-prevent-video-ios').play();
+    document.getElementById('sleep-prevent-video-android').play();
+}, 10000);
