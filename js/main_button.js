c14e9f40d86bb712f795bff022e2d0d78592bec1
Kei Kawamura <wzdmriv@g.ecc.u-tokyo.ac.jp>
2021-01-22 12:19:46 +0900

    s

--- js/main_button.js
+++ js/main_button.js
@@ -8,10 +8,24 @@ function getParam(name, url) {
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
-var room_id = getParam('room_id');
-var dbref = db.ref("/idList").child(room_id).child("config");
-dbref.on('value', (snapshot) =>{
-    const data = snapshot.val();
+function touchstart_he(){
     console.log("hello1");
-    console.log(data);
-});
+}
+function touchend_he(){
+    console.log("hello1");
+}
+
+window.onload = function() {
+    var room_id = getParam('room_id');
+    var dbref = db.ref("/idList").child(room_id).child("config");
+    dbref.on('value', (snapshot) =>{
+        const data = snapshot.val();
+        const data_json = Object.keys(data);
+        console.log("hello1");
+        alert(data_json[0]);
+    });
+    var he_button = document.getElementById("he_button");
+    he_button.addEventListener("touchstart",touchstart_he);
+    he_button.addEventListener("touchend",touchend_he);
+
+};
--- js/main_setting.js
+++ js/main_setting.js
@@ -4,11 +4,13 @@ function genURL(){
         var mail_address = document.forms.form1.mail_address.value;
         var url = "https://wzdmriv.github.io/heha/heha_button.html?room_id=" + room_id
         document.getElementById( "gend_url" ).value = url;
+
+        //QRコード表示
         var utf8qrtext = unescape(encodeURIComponent(url));
         $("#img-qr").html("");
         $("#img-qr2").html("");
         $("#img-qr").qrcode({width:200,height:200,text:utf8qrtext}); 
-        var canvas = $("#canvas")[0]; //#canvasからcanvas要素を取得。[0]をつけるのがポイント。
+        var canvas = $("#canvas")[0];
         var ctx_canvas = canvas.getContext("2d");
         var qr = ctx_canvas.getImageData(0,0,200,200);
         var img_qr1 = document.createElement("canvas");
@@ -19,10 +21,12 @@ function genURL(){
         ctx_canvas1.fillStyle = "rgb(255, 255, 255)";
         ctx_canvas1.fillRect(0,0,240,240);
         ctx_canvas1.putImageData(qr, 20, 20);
-        var img_src = img_qr1.toDataURL("image/png"); //toDataURLでpng形式に変換
-        $("#img-qr").html(''); //canvas要素を削除
+        var img_src = img_qr1.toDataURL("image/png");
+        $("#img-qr").html('');
         $("#img-qr1").html('');
-        $("#img-qr2").attr("src",img_src); //pngを表示
+        $("#img-qr2").attr("src",img_src);
+        
+        //データベース登録
         db.ref("/idList").child(room_id).child("config").set({mail:mail_address,time:10,weight:10,ha:"disable"});
     });
 }
